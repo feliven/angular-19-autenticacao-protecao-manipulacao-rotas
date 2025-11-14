374aec1ca3c0c6cb3ad2fc86cd73f41994164c89
FVS <feliven@gmail.com>
2025-11-14 11:46:49 -0300

    configura layout de página logada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,19 @@ import { Routes } from '@angular/router';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { ListaPostagemComponent } from './pages/lista-postagem/lista-postagem.component';
 import { autenticacaoGuard } from './guards/autenticacao.guard';
+import { LayoutComponent } from './components/layout/layout.component';
 
 export const routes: Routes = [
   { path: 'registro', component: RegistroComponent },
+  // {
+  //   path: 'posts',
+  //   canActivate: [autenticacaoGuard],
+  //   component: ListaPostagemComponent,
+  // },
   {
-    path: 'posts',
+    path: '',
+    component: LayoutComponent,
     canActivate: [autenticacaoGuard],
-    component: ListaPostagemComponent,
+    children: [{ path: 'posts', component: ListaPostagemComponent }],
   },
 ];
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
-import { RouterOutlet } from '../../../../node_modules/@angular/router/index';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
